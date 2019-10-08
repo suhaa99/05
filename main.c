6d4b65ec113f0a7fab4e8981d2679d340e263f27
suhaa99 <jjangsoo99@naver.com>
2019-10-08 15:30:19 +0900

    05

--- main.c
+++ main.c
@@ -5,15 +5,9 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i=0;
-	int meter ;
+	int i;
+	for( i=0 ; i<10 ; i++)
+	printf("Hello World!\n");
 	
-	while(i<3)
-	{
-		meter = i*1609 ;
-		printf("%d 마일은 %d 미터입니다.\n", i, meter);
-		i++;
-		
-	}
 	return 0;
 }
